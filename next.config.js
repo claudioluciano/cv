c08f579819d44c166a5b043e0d8ad287c75b6e11
claudioluciano <claudio_luciano@live.com>
2024-01-03 00:39:35 +0000

    Add basePath and enable reactStrictMode in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
+
+    reactStrictMode: true,
+
+    // Add basePath
+    basePath: '/cv',
 }
 
 module.exports = nextConfig
